117a598f517e7eab3ba65ce80037b26c905082aa
bwl <bwl15698296860@126.com>
2024-07-03 10:26:03 +0800

    update_embedding

--- tests/embedding_test/test_llama.py
+++ tests/embedding_test/test_llama.py
@@ -25,7 +25,8 @@ def llama_query():
     embed_model = SparkAiEmbeddingModel(spark_embedding_app_id=os.environ['SPARK_Embedding_APP_ID'],
                                         spark_embedding_api_key=os.environ['SPARK_Embedding_API_KEY'],
                                         spark_embedding_api_secret=os.environ['SPARK_Embedding_API_SECRET'],
-                                        spark_embedding_domain=os.environ['SPARKAI_Embedding_DOMAIN'])
+                                        spark_embedding_domain=os.environ['SPARKAI_Embedding_DOMAIN'],
+                                        QPS=2)
     # define LLM Model
     sparkai = SparkAI(
         spark_api_url=os.environ["SPARKAI_URL"],
